26be7c12eeb5a38572f0f985ac627061e8cef0cf
Luis <128272321+luisitossb@users.noreply.github.com>
2024-05-20 09:46:29 -0700

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,208 +1,148 @@
-const chartTypes = ['bar', 'line', 'stackedBar', 'histogram'];
-let currentChartIndex = 0;
-let chartInstance;
-let currentPage = 0;
-const itemsPerPage = 20;
+const chartTypes = ['line', 'bar'];
+let currentReleaseYearChartIndex = 0;
+let currentGenreChartIndex = 0;
+let currentAgeRangeChartIndex = 0;
+let releaseYearChart, genreChart, ageRangeChart;
 let allData = [];
-let filteredData = [];
 
 async function fetchData() {
     try {
-        const response = await fetch('https://datavizbackendfromphp.onrender.com/data');
+        const response = await fetch('https://datavizbackendfromphp.onrender.com/data'); // Update this to your backend URL
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
         const rawData = await response.json();
         allData = rawData;
-        applyFilters();
+        updateAllCharts();
     } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error('Error fetching data:', error.message);
     }
 }
 
 function parseCsvData(rawData, aspect) {
+    const validRatings = ["G", "PG", "PG-13", "R", "NC-17", "TV-Y", "TV-Y7", "TV-G", "TV-PG", "TV-14", "TV-MA", "NR"];
+    switch (aspect) {
+        case 'release_year':
+            return rawData.reduce((acc, record) => {
+                acc[record.release_year] = (acc[record.release_year] || 0) + 1;
+                return acc;
+            }, {});
+        case 'rating':
+            return rawData.reduce((acc, record) => {
+                if (validRatings.includes(record.rating)) {
+                    acc[record.rating] = (acc[record.rating] || 0) + 1;
+                }
+                return acc;
+            }, {});
+        case 'genre':
+            return rawData.reduce((acc, record) => {
+                record.listed_in.split(',').forEach(genre => {
+                    genre = genre.trim();
+                    acc[genre] = (acc[genre] || 0) + 1;
+                });
+                return acc;
+            }, {});
+        default:
+            return {};
+    }
+}
+
+function initializeChart(chartType, data, canvasId, aspect) {
+    const ctx = document.getElementById(canvasId).getContext('2d');
+
+    if (!data || !Object.keys(data).length) {
+        console.error('Invalid data for chart initialization');
+        return;
+    }
+
+    if (aspect === 'release_year' && releaseYearChart) {
+        releaseYearChart.destroy();
+    } else if (aspect === 'genre' && genreChart) {
+        genreChart.destroy();
+    } else if (aspect === 'rating' && ageRangeChart) {
+        ageRangeChart.destroy();
+    }
+
+    const colors = {
+        backgroundColor: 'rgba(229, 9, 20, 0.2)', // Light, transparent Netflix red
+        borderColor: 'rgba(229, 9, 20, 0.8)' // Darker, semi-transparent Netflix red for the border
+    };
+
+    const chartOptions = {
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+            legend: {
+                display: true,
+                position: 'top',
+            },
+            title: {
+                display: true,
+                text: `Chart Type: ${chartType.charAt(0).toUpperCase() + chartType.slice(1)}`
+            }
+        },
+        scales: {
+            x: {
+                ticks: {
+                    autoSkip: chartType === 'line' && aspect === 'release_year',
+                    maxTicksLimit: chartType === 'line' && aspect === 'release_year' ? Math.floor(Object.keys(data).length / 2) : undefined
+                }
+            },
+            y: {
+                beginAtZero: true
+            }
+        }
+    };
+
+    const newChart = new Chart(ctx, {
+        type: chartType,
+        data: {
+            labels: Object.keys(data),
+            datasets: [{
+                label: 'Count',
+                data: Object.values(data),
+                backgroundColor: colors.backgroundColor,
+                borderColor: colors.borderColor,
+                borderWidth: 1
+            }]
+        },
+        options: chartOptions
+    });
+
     if (aspect === 'release_year') {
-        const groupedData = rawData.reduce((acc, record) => {
-            const key = record.release_year;
-            acc[key] = (acc[key] || 0) + 1;
-            return acc;
-        }, {});
-
-        const labels = Object.keys(groupedData);
-        const values = Object.values(groupedData);
-
-        return { labels, values };
-    }
-
-    if (aspect === 'rating') {
-        const validRatings = ["G", "PG", "PG-13", "R", "NC-17", "TV-Y", "TV-Y7", "TV-G", "TV-PG", "TV-14", "TV-MA", "NR"];
-        const groupedData = rawData.reduce((acc, record) => {
-            const key = record.rating;
-            if (validRatings.includes(key)) {
-                acc[key] = (acc[key] || 0) + 1;
-            }
-            return acc;
-        }, {});
-
-        const labels = Object.keys(groupedData);
-        const values = Object.values(groupedData);
-
-        return { labels, values };
-    }
-
-    if (aspect === 'genre') {
-        const groupedData = rawData.reduce((acc, record) => {
-            const genres = record.listed_in.split(',').map(genre => genre.trim());
-            genres.forEach(genre => {
-                acc[genre] = (acc[genre] || 0) + 1;
-            });
-            return acc;
-        }, {});
-
-        const labels = Object.keys(groupedData);
-        const values = Object.values(groupedData);
-
-        return { labels, values };
-    }
-
-    return { labels: [], values: [] };
-}
-
-
-function initializeChart(chartType, data) {
-    const ctx = document.getElementById('chartCanvas').getContext('2d');
-
-    if (chartInstance) {
-        chartInstance.destroy();
-    }
-
-    if (chartType === 'stackedBar') {
-        chartInstance = new Chart(ctx, {
-            type: 'bar',
-            data: {
-                labels: data.labels,
-                datasets: [{
-                    label: 'Count',
-                    data: data.values,
-                    backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                    borderColor: 'rgba(75, 192, 192, 1)',
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                responsive: true,
-                maintainAspectRatio: false,
-                plugins: {
-                    legend: {
-                        display: true,
-                        position: 'top',
-                    },
-                    title: {
-                        display: true,
-                        text: `Chart Type: Stacked Bar Chart`
-                    }
-                },
-                scales: {
-                    x: {
-                        stacked: true,
-                    },
-                    y: {
-                        stacked: true,
-                        beginAtZero: true
-                    }
-                }
-            }
-        });
-    } else if (chartType === 'histogram') {
-        chartInstance = new Chart(ctx, {
-            type: 'bar',
-            data: {
-                labels: data.labels,
-                datasets: [{
-                    label: 'Count',
-                    data: data.values,
-                    backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                    borderColor: 'rgba(75, 192, 192, 1)',
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                responsive: true,
-                maintainAspectRatio: false,
-                plugins: {
-                    legend: {
-                        display: true,
-                        position: 'top',
-                    },
-                    title: {
-                        display: true,
-                        text: `Chart Type: Histogram`
-                    }
-                },
-                scales: {
-                    y: {
-                        beginAtZero: true
-                    }
-                }
-            }
-        });
-    } else {
-        chartInstance = new Chart(ctx, {
-            type: chartType,
-            data: {
-                labels: data.labels,
-                datasets: [{
-                    label: 'Count',
-                    data: data.values,
-                    backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                    borderColor: 'rgba(75, 192, 192, 1)',
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                responsive: true,
-                maintainAspectRatio: false,
-                plugins: {
-                    legend: {
-                        display: true,
-                        position: 'top',
-                    },
-                    title: {
-                        display: true,
-                        text: `Chart Type: ${chartType.charAt(0).toUpperCase() + chartType.slice(1)}`
-                    }
-                },
-                scales: {
-                    y: {
-                        beginAtZero: true,
-                        display: chartType !== 'pie' && chartType !== 'doughnut' && chartType !== 'radar'
-                    }
-                }
-            }
-        });
+        releaseYearChart = newChart;
+    } else if (aspect === 'genre') {
+        genreChart = newChart;
+    } else if (aspect === 'rating') {
+        ageRangeChart = newChart;
     }
 }
 
-function applyFilters() {
-    const aspectSelect = document.getElementById('aspectSelect').value;
-
-    filteredData = allData;
-
-    currentPage = 0;
-    updateChart(aspectSelect);
+function updateChart(aspect, canvasId, currentChartIndex) {
+    const data = parseCsvData(allData, aspect);
+    const chartType = chartTypes[currentChartIndex];
+    initializeChart(chartType, data, canvasId, aspect);
 }
 
-function updateChart(aspect) {
-    const data = parseCsvData(filteredData, aspect);
-    initializeChart(chartTypes[currentChartIndex], data);
+function updateAllCharts() {
+    updateChart('release_year', 'chartCanvasReleaseYear', currentReleaseYearChartIndex);
+    updateChart('genre', 'chartCanvasGenre', currentGenreChartIndex);
+    updateChart('rating', 'chartCanvasAgeRange', currentAgeRangeChartIndex);
 }
 
-document.getElementById('swapViewButton').addEventListener('click', function() {
-    currentChartIndex = (currentChartIndex + 1) % chartTypes.length;
-    const aspectSelect = document.getElementById('aspectSelect').value;
-    updateChart(aspectSelect);
+document.getElementById('swapViewButtonReleaseYear').addEventListener('click', function() {
+    currentReleaseYearChartIndex = (currentReleaseYearChartIndex + 1) % chartTypes.length;
+    updateChart('release_year', 'chartCanvasReleaseYear', currentReleaseYearChartIndex);
 });
 
-document.getElementById('aspectSelect').addEventListener('change', function() {
-    const aspectSelect = document.getElementById('aspectSelect').value;
-    applyFilters();
+document.getElementById('swapViewButtonGenre').addEventListener('click', function() {
+    currentGenreChartIndex = (currentGenreChartIndex + 1) % chartTypes.length;
+    updateChart('genre', 'chartCanvasGenre', currentGenreChartIndex);
+});
+
+document.getElementById('swapViewButtonAgeRange').addEventListener('click', function() {
+    currentAgeRangeChartIndex = (currentAgeRangeChartIndex + 1) % chartTypes.length;
+    updateChart('rating', 'chartCanvasAgeRange', currentAgeRangeChartIndex);
 });
 
 window.addEventListener('load', fetchData);
