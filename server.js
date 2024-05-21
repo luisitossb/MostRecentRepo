23d127992bf920e6e76d2644b2a53f4f18edf80f
bravo312 <aymanbaydoun7@gmail.com>
2024-05-20 20:42:31 -0700

    server.js changes

--- server.js
+++ server.js
@@ -1,25 +1,15 @@
 const express = require('express');
-const cors = require('cors');
-const fs = require('fs');
 const path = require('path');
-const csv = require('csv-parser');
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.use(cors());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'))); 
 
-app.get('/data', (req, res) => {
-    const results = [];
-    fs.createReadStream('netflix_titles.csv')
-        .pipe(csv())
-        .on('data', (data) => results.push(data))
-        .on('end', () => {
-            res.json(results);
-        });
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html')); // Update 'public' to the correct directory name
 });
 
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}/`);
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
